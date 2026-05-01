6725c40c9af567a484ed7f95166c3476e57d514c
Luis <banedycraft@gmail.com>
2026-04-30 23:01:26 -0300

    init gatway

---
+++ src/main/java/br/com/bali/code/healthcaregateway/HealthcareGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.bali.code.healthcaregateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bali/code/healthcaregateway/HealthcareGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bali.code.healthcaregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
